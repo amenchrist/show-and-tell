f29bac1bd74a49d3516a866c74a3ab2fbeb5e44b
amenchrist <amenphronesis@gmail.com>
2019-07-20 02:47:51 +0100

    Updates

---
+++ precache-manifest.c491e8b79915c0d63221e35bd3794c7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae6d6dc838a38ee4e11a7194f00963c",
+    "url": "/show-and-tell/index.html"
+  },
+  {
+    "revision": "d88a935687eb3942c4de",
+    "url": "/show-and-tell/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "5299bc6db712d742df96",
+    "url": "/show-and-tell/static/js/2.9635232a.chunk.js"
+  },
+  {
+    "revision": "d88a935687eb3942c4de",
+    "url": "/show-and-tell/static/js/main.9e53bbc4.chunk.js"
+  },
+  {
+    "revision": "258c05b93f578b815f2d",
+    "url": "/show-and-tell/static/js/runtime~main.6d3d50ea.js"
+  }
+]);
