73260fe4d4081562bf818d73c4f8c8e02a42f355
Amen Christ <amenphronesis@gmail.com>
2019-07-20 02:25:08 +0100

    Add files via upload
    Added Portfolio files

---
+++ build/precache-manifest.2f0d244f825ac439ef519a50bcb4453f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e9e1013f2e387589bb14bb30f77d82",
+    "url": "/show-and-tell/index.html"
+  },
+  {
+    "revision": "da88f306182bd8fc3f8d",
+    "url": "/show-and-tell/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5299bc6db712d742df96",
+    "url": "/show-and-tell/static/js/2.9635232a.chunk.js"
+  },
+  {
+    "revision": "da88f306182bd8fc3f8d",
+    "url": "/show-and-tell/static/js/main.9e53bbc4.chunk.js"
+  },
+  {
+    "revision": "258c05b93f578b815f2d",
+    "url": "/show-and-tell/static/js/runtime~main.6d3d50ea.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Album from './album';
+
+function App() {
+  return (
+    <div>
+      <Album />
+
+    </div>
+  );
+}
+
+export default App;
